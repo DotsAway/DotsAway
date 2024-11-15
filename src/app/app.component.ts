553eeec4cf7128f1aa5f7c17f4730c603ede93ed
Sejoslaw <k.dobrzynski94@gmail.com>
2024-11-15 17:03:32 +0100

    +footer template;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoComponent } from "./components/logo/logo.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'dotsaway-root',
   standalone: true,
-  imports: [RouterOutlet, LogoComponent],
+  imports: [RouterOutlet, LogoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
