582715116cc64cda3cf70122661ec7d120a5b4fd
Sejoslaw <k.dobrzynski94@gmail.com>
2024-11-15 20:16:35 +0100

    first menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,19 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'dotsaway-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  menuForm: FormGroup<{
+    width: FormControl<string | null>,
+    height: FormControl<string | null>,
+    lines: FormControl<string | null>
+  }>;
+
   constructor(
-    private router: Router
-  ) {}
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.menuForm = this.formBuilder.group({
+      width: ['', Validators.required],
+      height: ['', Validators.required],
+      lines: ['', Validators.required]
+    });
+  }
 
   goToSeed(seed: string | null): void {
     this.router.navigate(['/board', { seed }]);
+  }
+
+  onSubmit(): void {
+    if (this.menuForm.valid) {
+      // TODO: Generate seed, and go to board - start game
+    }
+    this.menuForm.reset();
   }
 }
