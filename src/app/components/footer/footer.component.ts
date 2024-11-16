71555931d75bcba71c6692fa8c34727a8b92f88b
Sejoslaw <k.dobrzynski94@gmail.com>
2024-11-16 14:13:28 +0100

    +footer;

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'dotsaway-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  protected repoLink: string = 'https://github.com/DotsAway/DotsAway';
 }
